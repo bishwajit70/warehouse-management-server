94ad47594d90e885553de4cddee1e396c8e9058e
bishwajit70 <mailtobishwa15@gmail.com>
2022-04-30 20:39:08 +0600

    DELETE is workign perfectly

--- index.js
+++ index.js
@@ -46,13 +46,13 @@ async function run() {
         })
 
 
-        // delete an Event 
-        // app.delete('/event/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) }
-        //     const result = await eventCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+        // delete an invetory item 
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await inventoryCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // changes 
         // app.get('/hero', (req, res)=>{
